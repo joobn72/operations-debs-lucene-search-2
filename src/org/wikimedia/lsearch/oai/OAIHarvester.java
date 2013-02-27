24eb3e33ebe867c2a763a44405571ac556426a36
Munagala Ramanath <ram@wikimedia.org>
2013-02-26 16:35:01 -0800

    Fix for bug 45266. Needs parallel changes to OAI.
    See change id: I07d5c2cedcd7550505b53d380d13111bd83e3216
    in OAI extension.
    
    Remove debug setting for bufferDocs and turn off debug flag; use TABs for indentation.
    
    Change-Id: I31bde27a7a64e0d9fff340843f56fe6c6d8a322a

--- src/org/wikimedia/lsearch/oai/OAIHarvester.java
+++ src/org/wikimedia/lsearch/oai/OAIHarvester.java
@@ -1,8 +1,13 @@
 package org.wikimedia.lsearch.oai;
 
 import java.io.BufferedInputStream;
-import java.io.IOException;
+import java.io.ByteArrayOutputStream;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.InputStream;
+import java.io.IOException;
+
 import java.net.Authenticator;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -32,6 +37,30 @@ public class OAIHarvester {
 	/** number of retries before giving up, useful when there are broken servers in the cluster */
 	protected int retries = 5;
 	
+	// for debugging
+	// save contents of input stream to memory stream and dump to file
+	private static final boolean DBG = false;
+	private static int fnum = 1;
+	public static InputStream toMem( InputStream is ) throws IOException {
+	    ByteArrayOutputStream os = new ByteArrayOutputStream();
+	    // open new dump file
+	    File dir = new File( "/var/tmp" );
+	    String
+		pfx = String.format( "oai_%d_", fnum++ ),
+		sfx = ".xml";
+	    File dumpfile = File.createTempFile( pfx, sfx, dir );
+	    FileOutputStream fos = new FileOutputStream( dumpfile );
+
+	    // read bytes
+	    int c;
+	    while ( -1 != (c = is.read()) ) {
+		os.write( c ); fos.write( c );
+	    }
+	    fos.close();
+	    ByteArrayInputStream bis = new ByteArrayInputStream( os.toByteArray() );
+	    return bis;
+	}  // toMem
+
 	public OAIHarvester(IndexId iid, String url, Authenticator auth) throws MalformedURLException{
 		this.urlbase = url;
 		this.iid = iid;
@@ -70,12 +99,19 @@ public class OAIHarvester {
 				// set some timeouts
 				urlConn.setReadTimeout(60 * 1000); // 60 seconds
 				urlConn.setConnectTimeout(60 * 1000); // 60 seconds
-				InputStream in = new BufferedInputStream(urlConn.getInputStream());
+				InputStream in;
+				if ( ! DBG ) {
+				    in = new BufferedInputStream( urlConn.getInputStream() );
+				} else {
+				    in = toMem( urlConn.getInputStream() );
+				}
+
 				parser = new OAIParser(in,collector);
 				parser.parse();
 				resumptionToken = parser.getResumptionToken();
 				responseDate = parser.getResponseDate();
 				in.close();
+				log.trace( String.format( "resumptionToken = %s, responseDate = %s", resumptionToken, responseDate ) );
 				break;
 			} catch(IOException e){				
 				if(tryNum == this.retries)
@@ -91,7 +127,8 @@ public class OAIHarvester {
 		ArrayList<IndexUpdateRecord> ret = new ArrayList<IndexUpdateRecord>();
 		try{			
 			do{
-				read(new URL(urlbase+"&verb=ListRecords&metadataPrefix=mediawiki&resumptionToken="+resumptionToken));
+				URL url = new URL( urlbase + "&verb=ListRecords&metadataPrefix=mediawiki&resumptionToken=" + getSequence() );
+				read( url );
 				ret.addAll(collector.getRecords());
 			} while(hasMore() && ret.size() < atLeast);
 		} catch(IOException e){
@@ -101,8 +138,29 @@ public class OAIHarvester {
 		return ret;
 	}
 	
+	/** Invoke ListRecords using the last resumption token, get atLeast num of records */
+	public ArrayList<IndexUpdateRecord> getRecordsSeq( String seq, int atLeast ) {
+		ArrayList<IndexUpdateRecord> ret = new ArrayList<IndexUpdateRecord>();
+		try{			
+			do{
+				read( new URL( urlbase + "&verb=ListRecords&metadataPrefix=mediawiki&resumptionToken=" + seq ) );
+				ret.addAll( collector.getRecords() );
+			} while( hasMore() && ret.size() < atLeast );
+		} catch( IOException e ){
+			log.warn( "I/O exception listing records: " + e.getMessage(), e );
+			return null;
+		}
+		return ret;
+	}
+	
 	public boolean hasMore(){
-		return !resumptionToken.equals("");
+		return resumptionToken.endsWith(":");
+	}
+	
+	public String getSequence(){
+		return resumptionToken.endsWith(":")
+			? resumptionToken.substring( 0, resumptionToken.length() - 1 )
+			: resumptionToken;
 	}
 	
 	public String getResponseDate(){
